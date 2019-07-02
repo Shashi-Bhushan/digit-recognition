d05eac8577d79de7086894aa6d5162e6cb0db174
Shashi Bhushan <sbhushan1@outlook.com>
2019-07-02 22:30:43 +0530

    Created SVM

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import datetime as dt
 from sklearn import datasets, svm, metrics
 # fetch original mnist dataset
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
 
 from util import *
 
@@ -20,7 +21,39 @@ def main():
     # it creates openml folder in your root project folder
     mnist = fetch_openml('mnist_784', version=1, data_home='./')
 
-    show_digits(mnist.data, mnist.target)
+    data = mnist.data
+    target = mnist.target
+
+    # show_random_digits(data, target)
+
+    # ---------------- classification begins -----------------
+    random_index = np.random.choice(data.shape[0], 10000)
+
+    # Random dataset classification
+    X = data[random_index]
+    y = target[random_index]
+
+    # Full dataset classification
+    # X = data
+    # y = target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=30)
+
+    # Create a SVM Classifier
+    svm_classifier = svm.SVC(C=5, gamma=0.5)
+
+    # Training SVM
+
+    print("Training SVM")
+    svm_classifier.fit(X_train, y_train)
+    print("Finished Training SVM")
+
+    # Testing SVM
+
+    y_predicted = svm_classifier.predict(X_test)
+
+    print("Classification Report: %s" % (metrics.classification_report(y_test, y_predicted)))
+    print("Accuracy : %s" % (metrics.accuracy_score(y_test, y_predicted)))
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -8,16 +8,16 @@ from matplotlib.colors import Normalize
 import numpy as np
 
 
-def show_digits(images, target, sample_size=10):
+def show_random_digits(images, target, sample_size=10):
     random_index = np.random.choice(images.shape[0], sample_size)
 
     images_with_labels = list(zip(images[random_index], target[random_index]))
 
-    img = plt.figure(1, figsize=(16, 20), dpi = 120)
+    plt.figure(1, figsize=(16, 20), dpi=120)
 
     for index, (image, label) in enumerate(images_with_labels):
         plt.subplot(np.ceil(sample_size/6.0), 6, index + 1)
         plt.axis('off')
-        plt.imshow(image.reshape(28, 28), cmap = plt.cm.gray_r, interpolation='nearest')
+        plt.imshow(image.reshape(28, 28), cmap=plt.cm.gray_r, interpolation='nearest')
         plt.title("Digit {}".format(label))
     plt.show()
